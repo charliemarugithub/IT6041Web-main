8a788f93d859278ce4ab50ffd63f83cf9e8f685d
Charlie Maru <charliemaru@gmail.com>
2020-11-30 15:04:15 +1300

    Back button on product_details page
    when adding item to cart (cart_total) updates, clicking 'Go Back' will automatically refresh previous page to show new updated cart-total in navbar

--- IT6041App/views.py
+++ IT6041App/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.mail.backends import console
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 import json
