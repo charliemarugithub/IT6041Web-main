1e5cafdcf5cbb376bfd41a1a6152cee375ff07b8
Charlie Maru <charliemaru@gmail.com>
2020-12-04 15:15:28 +1300

    Update urls.py

--- IT6041App/urls.py
+++ IT6041App/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('cart/', views.cart, name='cart'),
+    path('add_coupon', views.add_coupon, name='add_coupon'),
     path('checkout/', views.checkout, name="checkout"),
     path('staff/', views.staff, name='staff'),
     path('update_item/', views.updateItem, name='update_item'),
